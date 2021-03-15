8eb89387c882b1817da5d5e7776368a45f61fe88
Aman <amansg57@gmail.com>
2021-03-15 17:04:15 +0000

    Basic changes, page outline and graphing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Dropdown, Button, Navbar, Hero, Footer, Container, Content } from 'react-bulma-components';
+import {XYPlot, LineMarkSeries} from 'react-vis';
+import '../node_modules/react-vis/dist/style.css';
 
 function App() {
+  const data = [
+    {x: 0, y: 8},
+    {x: 1, y: 5},
+    {x: 2, y: 4},
+    {x: 3, y: 9},
+    {x: 4, y: 1},
+    {x: 5, y: 7},
+    {x: 6, y: 6},
+    {x: 7, y: 3},
+    {x: 8, y: 2},
+    {x: 9, y: 0}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar color="dark">
+        <Navbar.Menu>
+          <Navbar.Container>
+            <Navbar.Item href="#">
+              Home
+            </Navbar.Item>
+            <Navbar.Item href="#">
+              ???
+            </Navbar.Item>
+          </Navbar.Container>
+          <Navbar.Container position="end">
+            <Navbar.Item href="#">
+              About
+            </Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+
+      <Hero size="medium">
+        <Hero.Body>
+
+        <XYPlot height={450} width={1200}>
+          <LineMarkSeries data={data} />
+        </XYPlot>
+
+        </Hero.Body>
+      </Hero>
+
+        <Dropdown>
+          <Dropdown.Item value="item">
+            Dropdown item
+          </Dropdown.Item>
+        </Dropdown>
+
+        <Button color='danger'>
+          Button
+        </Button>
+
+        
+
+      <Hero.Footer>
+        <Footer>
+          <Container>
+            <Content style={{ textAlign: 'center' }}>
+              <p>
+                <strong>FFXIV Meta</strong> by <a href="#">Aman Gill</a>. The github link for this site is 
+                <a href="#"> here</a>.
+              </p>
+            </Content>
+          </Container>
+        </Footer>
+      </Hero.Footer>
+
     </div>
+    
   );
 }
 
